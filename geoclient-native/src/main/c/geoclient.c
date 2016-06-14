3448c08e6c44e419162dae7160eafdeec4663b0d
Matthew Lipper <mlipper@doitt.nyc.gov>
2016-06-14 16:31:14 -0400

    Works on my machine! (e.g., "Fork me not!")
    Working WIN64 native build that needs some rebasin' love prior to merge.
    Fork at your own risk.

--- geoclient-native/src/main/c/geoclient.c
+++ geoclient-native/src/main/c/geoclient.c
@@ -3,6 +3,6 @@
 /*
  * Simple proxy for platform-dependent Geosupport function call
  */
-void callgeo(char *work_area1, char *work_area2) {
+GEOCLIENT_API void callgeo(char *work_area1, char *work_area2) {
 	GEOSUPPORT_FUNC(work_area1, work_area2);
 }
--- geoclient-native/src/main/headers/geoclient.h
+++ geoclient-native/src/main/headers/geoclient.h
@@ -3,12 +3,17 @@
 
 // The Geosupport C API uses platform-dependent function names:
 #ifdef _WIN32
+  // When compiling with mingw64 on Windows (e.g., MSYS2 MINGW64 Shell)
+  // make sure to have gcc tell the linker to add the WIN32 standard call
+  // alias: '-Wl,--add-stdcall-alias'. This should result in the preprocessor
+  // adding '__stdcall' and a space just before the resolved method name.
   #include "NYCgeo.h"
   #define GEOSUPPORT_FUNC(work_area1, work_area2) NYCgeo(work_area1, work_area2);
   #ifdef DLL_EXPORT
-    # define GEOCLIENT_API __declspec(dllexport)
+    #define GEOCLIENT_API __declspec(dllexport)
   #else
-    # define GEOCLIENT_API __declspec(dllimport)
+    //#define GEOCLIENT_API __declspec(dllimport)
+    #define GEOCLIENT_API
   #endif
 #else
   #include "geo.h"
@@ -16,23 +21,11 @@
   #define GEOCLIENT_API
 #endif
 
-// The following ifdef block is the standard way of creating macros which make exporting
-// from a DLL simpler. All files within this DLL are compiled with the GEOCLIENT_EXPORT
-// symbol defined on the command line by the Gradle build. This symbol should not be
-// defined on any project that uses this DLL. This way any other project whose source
-// files include this file see GEOCLIENT_API functions as being imported from a DLL,
-// whereas this DLL sees symbols defined with this macro as being exported.
-// #if defined(_WIN32) && defined(DLL_EXPORT)
-// #define GEOCLIENT_API __declspec(dllexport)
-// #else
-// #define GEOCLIENT_API __declspec(dllimport)
-// #endif
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-GEOCLIENT_API void callgeo(char *work_area1, char *work_area2);
+  GEOCLIENT_API void callgeo(char *work_area1, char *work_area2);
 
 #ifdef __cplusplus
 }
