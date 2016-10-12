ec27f29bd280f7f2824c9e835ea8e513a7f1cc09
Matthew Lipper <mlipper@doitt.nyc.gov>
2016-10-12 14:52:35 -0400

    Fix broken Javadoc

--- geoclient-service/src/main/java/gov/nyc/doitt/gis/geoclient/service/web/JsonpAdvice.java
+++ geoclient-service/src/main/java/gov/nyc/doitt/gis/geoclient/service/web/JsonpAdvice.java
@@ -10,9 +10,10 @@ import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpRespon
  * Per the Spring Framework reference documentation (section 22.3.3) for JSONP
  * view resolution when using Jackson.
  * 
- * @see the <a href=
- *      "http://docs.spring.io/spring/docs/current/spring-framework-reference/htmlsingle/#mvc-ann-jsonp">
- *      Spring Framework Reference documentation</a>
+ * See the
+ * <a href="http://docs.spring.io/spring/docs/current/spring-framework-reference/htmlsingle/#mvc-ann-jsonp">Spring Framework Reference documentation</a>
+ * for a full explanation.
+ *
  * @author mlipper
  * @since 2.0.0
  */
@@ -21,9 +22,8 @@ public class JsonpAdvice extends AbstractJsonpResponseBodyAdvice {
 
   /**
    * Constructs a new {@link JsonpAdvice} instance which relies on a query
-   * parameter named {@code callback}.
+   * parameter named {@code callback} in the Ajax request from the browser.
    * 
-   * @param queryParamNames
    */
   public JsonpAdvice() {
     super("callback");
