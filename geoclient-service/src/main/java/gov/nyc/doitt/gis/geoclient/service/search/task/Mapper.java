6893ea6972f928fd8cad1bb3bba7e5d6faa3c3f7
Matthew Lipper <mlipper@doitt.nyc.gov>
2016-11-03 13:49:50 -0400

    Unfinished refactoring

---
+++ geoclient-service/src/main/java/gov/nyc/doitt/gis/geoclient/service/search/task/Mapper.java
@@ -0,0 +1,7 @@
+package gov.nyc.doitt.gis.geoclient.service.search.task;
+
+public interface Mapper
+{
+    void map(Object source, Object destination);
+
+}
---
+++ geoclient-service/src/main/java/gov/nyc/doitt/gis/geoclient/service/search/task/SpringMapper.java
@@ -0,0 +1,11 @@
+package gov.nyc.doitt.gis.geoclient.service.search.task;
+
+public class SpringMapper
+{
+
+    public SpringMapper()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+}
