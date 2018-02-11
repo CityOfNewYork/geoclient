0643756f30109e58050b97922d36a9aebc0e6a45
Matthew Lipper <mlipper@gmail.com>
2018-02-10 22:01:20 -0500

    Docker script tweaks

--- docker-build.sh
+++ docker-build.sh
@@ -1,18 +1,78 @@
 #!/usr/bin/env bash
 
-# Runs Geoclient build using Gradle's remote JVM debugging port
-# docker run \
-#   -it \
-#   --volumes-from geosupport \
-#   -v "$PWD":/home/gradle/geoclient \
-#   --name geoclient mlipper/geoclient:latest \
-#   "$@"
+#
+# TODO Replace with a Docker plugin for Gradle
+#
 
-#  -w /home/gradle/geoclient \
-#  -e GEOFILES=/opt/geosupport/fls/ \
-#  -e GS_LIBRARY_PATH=/opt/geosupport/lib \
-#  -e LD_LIBRARY_PATH=/opt/geosupport/lib:/home/gradle/geoclient/build/libs \
+USAGE=$(cat <<USAGE
 
-docker start -i geoclient
+Usage:
+        $0 run [cmd]
+    OR
+        $0 debug [cmd]
+    OR
+        $0 start
 
-# gradle -Dorg.gradle.debug=true
+USAGE
+)
+
+if [ "$#" == "0" ]; then
+  echo "$USAGE"
+  exit 1
+fi
+
+action=$1
+
+shift
+
+#
+# Create geoclient container from mlipper/geoclient:latest images.
+# Assumes a data volume container has been created called 'geosupport'.
+#
+function start {
+  docker start \
+    -i \
+    geoclient
+}
+
+#
+# Create geoclient container from mlipper/geoclient:latest images.
+# Assumes a data volume container has been created called 'geosupport'.
+#
+function run {
+  docker run \
+    -it \
+    --volumes-from geosupport \
+    -v "$PWD":/home/gradle/geoclient \
+    --name geoclient \
+    mlipper/geoclient:latest \
+    "$@"
+}
+
+function debug {
+  docker run \
+    --rm \
+    -it \
+    -p 5005:5005 \
+    -e GRADLE_OPTS="-Dorg.gradle.debug=true" \
+    --volumes-from geosupport \
+    -v "$PWD":/home/gradle/geoclient \
+    --name geoclient \
+    mlipper/geoclient:latest \
+    ./gradlew "$@"
+}
+
+case $action in
+  start)
+    start "$@"
+    ;;
+  run)
+    run "$@"
+    ;;
+  debug)
+    debug "$@"
+    ;;
+  *) echo "Error: unrecogized action $action"
+     exit 1
+     ;;
+esac
