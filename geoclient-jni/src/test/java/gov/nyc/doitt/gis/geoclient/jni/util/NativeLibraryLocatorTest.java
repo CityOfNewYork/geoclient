086bf3646bc6f95ceaac70575f8cc58d6a514655
Matthew Lipper <mlipper@doitt.nyc.gov>
2018-11-16 16:59:06 -0500

    [BROKEN] Javadoc broken

---
+++ geoclient-jni/src/test/java/gov/nyc/doitt/gis/geoclient/jni/util/NativeLibraryLocatorTest.java
@@ -0,0 +1,34 @@
+package gov.nyc.doitt.gis.geoclient.jni.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NativeLibraryLocatorTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testFind() {
+		fail("Not yet implemented");
+	}
+
+}
