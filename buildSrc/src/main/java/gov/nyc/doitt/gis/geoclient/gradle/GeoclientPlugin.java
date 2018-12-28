2ce016d021ce15c6a7ec50515541a6bca649bbda
Matthew Lipper <mlipper@doitt.nyc.gov>
2018-12-28 17:17:16 -0500

    [BROKEN] Almost working buildSrc plugin

--- buildSrc/src/main/java/gov/nyc/doitt/gis/geoclient/gradle/GeoclientPlugin.java
+++ buildSrc/src/main/java/gov/nyc/doitt/gis/geoclient/gradle/GeoclientPlugin.java
@@ -6,7 +6,9 @@ import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.testing.Test;
 public class GeoclientPlugin implements Plugin<Project> {
     public static final String ENV_VAR_GEOFILES = "GEOFILES";
+    public static final String ENV_VAR_GS_HOME = "GEOSUPPORT_HOME";
     public static final String PROP_GC_JNI_VERSION = "gcJniVersion";
+    public static final String PROP_GS_HOME = "gsHome";
     public static final String PROP_GS_GEOFILES = "gsGeofiles";
     public static final String PROP_GS_LIBRARY_PATH = "gsLibraryPath";
     public static final String SYSPROP_GC_JNI_VERSION = "gc.jni.version";
@@ -14,6 +16,7 @@ public class GeoclientPlugin implements Plugin<Project> {
     public void apply(Project project) {
         final GeosupportExtension geosupportExtension = project.getExtensions().create("geosupport", GeosupportExtension.class, project);
         final BuildConfigurationResolver resolver = new BuildConfigurationResolver(project);
+//        resolveGeosupportHome(project, geosupportExtension, resolver);
         project.getTasks().withType(Test.class).configureEach(new Action<Test>() {
             public void execute(Test test) {
                 test.systemProperty(SYSPROP_GC_JNI_VERSION, resolver.resolve(PROP_GC_JNI_VERSION, SYSPROP_GC_JNI_VERSION, null));
@@ -23,5 +26,24 @@ public class GeoclientPlugin implements Plugin<Project> {
                 test.environment(ENV_VAR_GEOFILES, resolver.resolveFromEnv(PROP_GS_GEOFILES, ENV_VAR_GEOFILES, geofiles.getOrNull()));
             }
         });
+    }
+
+    boolean resolveGeosupportHome(Project project, GeosupportExtension geosupportExtension, BuildConfigurationResolver resolver) {
+        String defaultValue = geosupportExtension.getHome().getOrNull();
+        // 
+        String geosupportHome = resolver.resolve(PROP_GS_HOME, ENV_VAR_GS_HOME, null);
+        return (geosupportHome != null && !geosupportHome.isEmpty());
+    }
+    boolean resolveGeosupportLibraryPath(Project project, GeosupportExtension geosupportExtension, BuildConfigurationResolver resolver) {
+        String defaultValue = null;
+        if(project.hasProperty(PROP_GS_HOME)) {
+            defaultValue = new File(project.property(PROP_GS_HOME), "lib").getCanonicalPath();
+        }
+        String geosupportLibraryPath = resolver.resolve(PROP_GS_HOME, ENV_VAR_GS_HOME, null);
+        return false;
+    }
+    boolean resolveGeosupportGeofiles(Project project, GeosupportExtension geosupportExtension, BuildConfigurationResolver resolver) {
+        
+        return false;
     }
 }
--- buildSrc/src/main/java/gov/nyc/doitt/gis/geoclient/gradle/GeosupportExtension.java
+++ buildSrc/src/main/java/gov/nyc/doitt/gis/geoclient/gradle/GeosupportExtension.java
@@ -2,16 +2,23 @@ package gov.nyc.doitt.gis.geoclient.gradle;
 import org.gradle.api.Project;
 import org.gradle.api.provider.Property;
 public class GeosupportExtension {
-    private final Property<String> libraryPath;
+    
     private final Property<String> geofiles;
+    private final Property<String> home;
+    private final Property<String> libraryPath;
+    
     public GeosupportExtension(Project project) {
         this.geofiles = project.getObjects().property(String.class);
+        this.home = project.getObjects().property(String.class);
         this.libraryPath = project.getObjects().property(String.class);
-    }
-    public Property<String> getLibraryPath() {
-        return this.libraryPath;
     }
     public Property<String> getGeofiles() {
         return this.geofiles;
+    }
+    public Property<String> getHome() {
+        return this.home;
+    }
+    public Property<String> getLibraryPath() {
+        return this.libraryPath;
     }
 }
