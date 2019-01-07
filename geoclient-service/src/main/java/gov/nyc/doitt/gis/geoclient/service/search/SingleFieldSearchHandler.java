d23d28310e9e67a00d81a03c10d855ba233cd1d9
Matthew Lipper <mlipper@doitt.nyc.gov>
2019-01-07 14:36:27 -0500

    Line endings

--- geoclient-service/src/main/java/gov/nyc/doitt/gis/geoclient/service/search/SingleFieldSearchHandler.java
+++ geoclient-service/src/main/java/gov/nyc/doitt/gis/geoclient/service/search/SingleFieldSearchHandler.java
@@ -34,100 +34,100 @@ import org.slf4j.Logger;
 
 public class SingleFieldSearchHandler
 {
-	private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(SingleFieldSearchHandler.class);
-	private final SearchId searchId;
-	private final LocationTokenizer locationTokenizer;
-	private final SearchTaskFactory searchTaskFactory;
-	private final ExecutorService executorService = Executors.newFixedThreadPool(4);
+  private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(SingleFieldSearchHandler.class);
+  private final SearchId searchId;
+  private final LocationTokenizer locationTokenizer;
+  private final SearchTaskFactory searchTaskFactory;
+  private final ExecutorService executorService = Executors.newFixedThreadPool(4);
 
-	public SingleFieldSearchHandler(SearchId searchId, LocationTokenizer locationTokenizer, SearchTaskFactory searchTaskFactory)
-	{
-		super();
-		this.searchId = searchId;
-		this.locationTokenizer = locationTokenizer;
-		this.searchTaskFactory = searchTaskFactory;
-	}
-	
-	public SearchResult findLocation(String inputString)
-	{
-		return findLocation(new SearchPolicy(), inputString);
-	}
-	
-	public SearchResult findLocation(SearchPolicy searchPolicy, String inputString)
-	{
-		Input input = new Input(searchId.next(), inputString);
-		LOGGER.info("Searching for {}", input);
-		LocationTokens locationTokens = locationTokenizer.parse(input);
-		// Initial search
-		SearchResult searchResult = doInitialSearch(searchPolicy, locationTokens);
-		if(searchResult.isExactMatch())
-		{
-			// We're done
-			log(searchResult);
-			return searchResult;
-		}
-		// Sub-searches
-		doSubSearches(searchResult);
-		log(searchResult);
-		return searchResult;
-	}
-	
-	protected SearchResult doInitialSearch(SearchPolicy searchPolicy, LocationTokens locationTokens)
-	{
-		List<SearchTask> searchTasks = this.searchTaskFactory.buildInitialSearchTasks(searchPolicy, locationTokens);
-		SearchResult searchResult = new SearchResult(searchPolicy, locationTokens);
-		doSearches(searchResult, searchTasks);
-		return searchResult;
-	}
-	
-	protected void doSubSearches(SearchResult searchResult)
-	{
-		List<SearchTask> subSearches = this.searchTaskFactory.buildSubsearchTasks(searchResult);
-		while(!subSearches.isEmpty() && !searchResult.isExactMatch())
-		{
-			doSearches(searchResult, subSearches);
-			subSearches = this.searchTaskFactory.buildSubsearchTasks(searchResult);
-		}
-	}
-	
-	protected void doSearches(SearchResult searchResult, List<SearchTask> searchTasks)
-	{
-		for (SearchTask searchTask : searchTasks)
-		{
-			Search search =  doSearch(searchTask);
-			LOGGER.debug("Search {}", search);
-			searchResult.add(search);
-		}
-	}
-	
-	protected Search doSearch(SearchTask searchTask)
-	{
-		Future<Search> future = this.executorService.submit(searchTask);
-		try
-		{
-			return future.get();
-		} catch (InterruptedException e)
-		{
-			Thread.currentThread().interrupt();
-			future.cancel(true);
-			throw new RuntimeException(e.getCause());
-		} catch (ExecutionException executionException)
-		{
-			throw new RuntimeException(executionException.getCause());
-		}
-	}
-	
-	@PreDestroy
-	public void releasePool()
-	{
-		if(this.executorService !=null)
-		{
-			this.executorService.shutdown();
-		}
-	}
-	
-	private void log(SearchResult searchResult)
-	{
-		LOGGER.info("Result for id: {} = {}", searchResult.getId(), searchResult);
-	}
+  public SingleFieldSearchHandler(SearchId searchId, LocationTokenizer locationTokenizer, SearchTaskFactory searchTaskFactory)
+  {
+    super();
+    this.searchId = searchId;
+    this.locationTokenizer = locationTokenizer;
+    this.searchTaskFactory = searchTaskFactory;
+  }
+
+  public SearchResult findLocation(String inputString)
+  {
+    return findLocation(new SearchPolicy(), inputString);
+  }
+
+  public SearchResult findLocation(SearchPolicy searchPolicy, String inputString)
+  {
+    Input input = new Input(searchId.next(), inputString);
+    LOGGER.info("Searching for {}", input);
+    LocationTokens locationTokens = locationTokenizer.parse(input);
+    // Initial search
+    SearchResult searchResult = doInitialSearch(searchPolicy, locationTokens);
+    if(searchResult.isExactMatch())
+    {
+      // We're done
+      log(searchResult);
+      return searchResult;
+    }
+    // Sub-searches
+    doSubSearches(searchResult);
+    log(searchResult);
+    return searchResult;
+  }
+
+  protected SearchResult doInitialSearch(SearchPolicy searchPolicy, LocationTokens locationTokens)
+  {
+    List<SearchTask> searchTasks = this.searchTaskFactory.buildInitialSearchTasks(searchPolicy, locationTokens);
+    SearchResult searchResult = new SearchResult(searchPolicy, locationTokens);
+    doSearches(searchResult, searchTasks);
+    return searchResult;
+  }
+
+  protected void doSubSearches(SearchResult searchResult)
+  {
+    List<SearchTask> subSearches = this.searchTaskFactory.buildSubsearchTasks(searchResult);
+    while(!subSearches.isEmpty() && !searchResult.isExactMatch())
+    {
+      doSearches(searchResult, subSearches);
+      subSearches = this.searchTaskFactory.buildSubsearchTasks(searchResult);
+    }
+  }
+
+  protected void doSearches(SearchResult searchResult, List<SearchTask> searchTasks)
+  {
+    for (SearchTask searchTask : searchTasks)
+    {
+      Search search =  doSearch(searchTask);
+      LOGGER.debug("Search {}", search);
+      searchResult.add(search);
+    }
+  }
+
+  protected Search doSearch(SearchTask searchTask)
+  {
+    Future<Search> future = this.executorService.submit(searchTask);
+    try
+    {
+      return future.get();
+    } catch (InterruptedException e)
+    {
+      Thread.currentThread().interrupt();
+      future.cancel(true);
+      throw new RuntimeException(e.getCause());
+    } catch (ExecutionException executionException)
+    {
+      throw new RuntimeException(executionException.getCause());
+    }
+  }
+
+  @PreDestroy
+  public void releasePool()
+  {
+    if(this.executorService !=null)
+    {
+      this.executorService.shutdown();
+    }
+  }
+
+  private void log(SearchResult searchResult)
+  {
+    LOGGER.info("Result for id: {} = {}", searchResult.getId(), searchResult);
+  }
 }
