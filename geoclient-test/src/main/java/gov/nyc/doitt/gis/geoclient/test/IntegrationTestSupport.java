c40e140fff831d8f2d28832b8cd148626e8f793f
Matthew Lipper <mlipper@doitt.nyc.gov>
2019-02-07 12:44:03 -0500

    Remove use of local custom plugin
    Plugin "works" but the DSL is not what it should be. This commit
    temporarily replaces use of the geoclient-plugin (under
    $projectDir/plugin) with direct test configuration.
    
    Note the tag of this commit's parent, PLUGIN_PAUSE, marking the last
    change where the plugin was configured for use by the main build.

--- geoclient-test/src/main/java/gov/nyc/doitt/gis/geoclient/test/IntegrationTestSupport.java
+++ geoclient-test/src/main/java/gov/nyc/doitt/gis/geoclient/test/IntegrationTestSupport.java
@@ -3,6 +3,7 @@ package gov.nyc.doitt.gis.geoclient.test;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -11,43 +12,48 @@ import java.util.TreeMap;
 import org.slf4j.Logger;
 import org.slf4j.event.Level;
 
-import gov.nyc.doitt.gis.geoclient.test.LogLevelAdapter;
-
-
 public interface IntegrationTestSupport {
 
-	default String getJavaLibraryPath() {
-		return System.getProperty("java.library.path");
-	}
-	
-	default String getJavaIoTmpdir() {
-		return System.getProperty("java.io.tmpdir");
-	}
-	
-	default boolean contains(File target) {
-		List<String> paths = Arrays.asList(System.getProperty("PATH").split(File.separator));
-		for (String string : paths) {
-			Path path = Paths.get(string);
-			if(path.toFile().equals(target)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	default File getSystemPropertyAsFile(String key) {
-		return new File(System.getProperty(key,"."));		
-	}
-	
-	default File getEnvVarAsFile(String name) {
-		return new File(System.getenv(name));		
-	}
-	
-	default void logEnvironment(final Level level, final Logger logger) {
-		LogLevelAdapter.logAll(level, logger, Collections.synchronizedSortedMap(new TreeMap<>(System.getenv())));
-	}
+    default String getJavaLibraryPath() {
+        return System.getProperty("java.library.path");
+    }
 
-	default void logSystemProperties(final Level level, final Logger logger) {
-		LogLevelAdapter.logAll(level, logger, Collections.synchronizedSortedMap(new TreeMap<>(System.getProperties())));
-	}
+    default String getJavaIoTmpdir() {
+        return System.getProperty("java.io.tmpdir");
+    }
+
+    default boolean contains(File target) {
+        List<String> paths = Arrays.asList(System.getProperty("PATH").split(File.separator));
+        for (String string : paths) {
+            Path path = Paths.get(string);
+            if (path.toFile().equals(target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    default List<File> filesFromPathString(String key) {
+        if (key == null) {
+            throw new NullPointerException("Path string argument cannot be null");
+        }
+        List<File> result = new ArrayList<>();
+        String[] paths = key.split(File.pathSeparator);
+        for (int i = 0; i < paths.length; i++) {
+            result.add(new File(paths[i]));
+        }
+        return result;
+    }
+
+    default File getEnvVarAsFile(String name) {
+        return new File(System.getenv(name));
+    }
+
+    default void logEnvironment(final Level level, final Logger logger) {
+        LogLevelAdapter.logAll(level, logger, Collections.synchronizedSortedMap(new TreeMap<>(System.getenv())));
+    }
+
+    default void logSystemProperties(final Level level, final Logger logger) {
+        LogLevelAdapter.logAll(level, logger, Collections.synchronizedSortedMap(new TreeMap<>(System.getProperties())));
+    }
 }
--- geoclient-test/src/test/java/gov/nyc/doitt/gis/geoclient/test/IntegrationTestSupportTest.java
+++ geoclient-test/src/test/java/gov/nyc/doitt/gis/geoclient/test/IntegrationTestSupportTest.java
@@ -1,9 +1,13 @@
 package gov.nyc.doitt.gis.geoclient.test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -13,43 +17,62 @@ import org.slf4j.event.Level;
 // TODO Capture stdout or mock
 class IntegrationTestSupportTest {
 
-	private static class IntegrationTest implements IntegrationTestSupport {
-		
-		final Logger logger = LoggerFactory.getLogger(IntegrationTest.class);
-		
-		void debugLogEnvironment() {
-			logEnvironment(Level.DEBUG, logger);
-		}
-		void debugLogSystemProperties() {
-			logSystemProperties(Level.DEBUG, logger);
-		}
-		
-		File getJavaLibraryPathAsFile() {
-			return getSystemPropertyAsFile(getJavaLibraryPath());
-		}
-	}
-	
-	private IntegrationTest itestInstance;
-	
-	@BeforeEach
-	void setUp() {
-		this.itestInstance = new IntegrationTest();
-	}
-	
-	@Test
-	void testGetSystemPropertyAsFile() {
-		File actual = itestInstance.getJavaLibraryPathAsFile();
-		assertTrue(actual.exists());
-	}
-	
-	@Test
-	void testLogEnvironment() {
-		itestInstance.debugLogEnvironment();
-	}
+    private static class IntegrationTest implements IntegrationTestSupport {
 
-	@Test
-	void testLogSystemProperties() {
-		itestInstance.debugLogSystemProperties();
-	}
+        final Logger logger = LoggerFactory.getLogger(IntegrationTest.class);
+
+        void debugLogEnvironment() {
+            logEnvironment(Level.DEBUG, logger);
+        }
+
+        void debugLogSystemProperties() {
+            logSystemProperties(Level.DEBUG, logger);
+        }
+
+        List<File> getJavaLibraryPathFiles() {
+            String javaLibraryPath = getJavaLibraryPath();
+            if (javaLibraryPath == null) {
+                return Collections.emptyList();
+            }
+            return filesFromPathString(getJavaLibraryPath());
+        }
+    }
+
+    private IntegrationTest itestInstance;
+    private String existingJavaLibraryPath;
+
+    @BeforeEach
+    void setUp() {
+        this.itestInstance = new IntegrationTest();
+        this.existingJavaLibraryPath = System.getProperty("java.library.path");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setProperty("java.library.path", existingJavaLibraryPath);
+    }
+
+    @Test
+    void testGetSystemPropertyAsFile() {
+        File javaHome = new File(System.getProperty("java.home"));
+        File javaTmpDir = new File(System.getProperty("java.io.tmpdir"));
+        String expectedPath = javaHome + File.pathSeparator + javaTmpDir;
+        System.setProperty("java.library.path", expectedPath);
+        List<File> actual = itestInstance.getJavaLibraryPathFiles();
+        assertFalse(actual.isEmpty());
+        assertEquals(2, actual.size());
+        assertEquals(actual.get(0), javaHome);
+        assertEquals(actual.get(1), javaTmpDir);
+    }
+
+    @Test
+    void testLogEnvironment() {
+        itestInstance.debugLogEnvironment();
+    }
+
+    @Test
+    void testLogSystemProperties() {
+        itestInstance.debugLogSystemProperties();
+    }
 
 }
