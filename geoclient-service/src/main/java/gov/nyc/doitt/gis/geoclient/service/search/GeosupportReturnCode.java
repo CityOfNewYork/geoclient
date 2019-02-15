d0725a7d6c5f3cf260364a34171f8d429a736349
Matthew Lipper <mlipper@doitt.nyc.gov>
2019-02-15 17:15:25 -0500

    Fix mapping exception by adding equals & hashCode
    Made change on a hunch because of Dozer's flaky reliance on reflection.
    Strangely it seems to work.
    
    Files reformatted using Eclipse Java formatter.

--- geoclient-service/src/main/java/gov/nyc/doitt/gis/geoclient/service/search/GeosupportReturnCode.java
+++ geoclient-service/src/main/java/gov/nyc/doitt/gis/geoclient/service/search/GeosupportReturnCode.java
@@ -17,76 +17,98 @@ package gov.nyc.doitt.gis.geoclient.service.search;
 
 import gov.nyc.doitt.gis.geoclient.config.ReturnCodeValue;
 
-public class GeosupportReturnCode
-{
-	private String returnCode;
-	private String reasonCode;
-	private String message;
+public class GeosupportReturnCode {
+    private String returnCode;
+    private String reasonCode;
+    private String message;
 
-	public boolean isCompassDirectionRequired()
-	{
-		return ReturnCodeValue.COMPASS_DIRECTION_REQUIRED.is(this.returnCode);
-	}
-	
-	public boolean isRejected()
-	{
-		if(ReturnCodeValue.SUCCESS.is(this.returnCode) || ReturnCodeValue.WARN.is(this.returnCode))
-		{
-			return false;
-		}
-		return true;
-	}
-	
-	public boolean hasSimilarNames()
-	{
-		return ReturnCodeValue.NOT_RECOGNIZED_WITH_SIMILAR_NAMES.is(this.returnCode);
-	}
-	
-	public boolean hasReasonCode()
-	{
-		return this.reasonCode != null;
-	}
-	
-	public boolean hasMessage()
-	{
-		return this.message != null;
-	}
-	
-	public String getReturnCode()
-	{
-		return returnCode;
-	}
+    public boolean isCompassDirectionRequired() {
+        return ReturnCodeValue.COMPASS_DIRECTION_REQUIRED.is(this.returnCode);
+    }
 
-	public void setReturnCode(String geosupportReturnCode)
-	{
-		this.returnCode = geosupportReturnCode;
-	}
+    public boolean isRejected() {
+        if (ReturnCodeValue.SUCCESS.is(this.returnCode) || ReturnCodeValue.WARN.is(this.returnCode)) {
+            return false;
+        }
+        return true;
+    }
 
-	public String getReasonCode()
-	{
-		return reasonCode;
-	}
+    public boolean hasSimilarNames() {
+        return ReturnCodeValue.NOT_RECOGNIZED_WITH_SIMILAR_NAMES.is(this.returnCode);
+    }
 
-	public void setReasonCode(String reasonCode)
-	{
-		this.reasonCode = reasonCode;
-	}
+    public boolean hasReasonCode() {
+        return this.reasonCode != null;
+    }
 
-	public String getMessage()
-	{
-		return message;
-	}
+    public boolean hasMessage() {
+        return this.message != null;
+    }
 
-	public void setMessage(String message)
-	{
-		this.message = message;
-	}
+    public String getReturnCode() {
+        return returnCode;
+    }
 
-	@Override
-	public String toString()
-	{
-		return "GeosupportReturnCode [returnCode=" + returnCode + ", reasonCode=" + reasonCode
-				+ ", message=" + message + "]";
-	}
+    public void setReturnCode(String geosupportReturnCode) {
+        this.returnCode = geosupportReturnCode;
+    }
 
+    public String getReasonCode() {
+        return reasonCode;
+    }
+
+    public void setReasonCode(String reasonCode) {
+        this.reasonCode = reasonCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "GeosupportReturnCode [returnCode=" + returnCode + ", reasonCode=" + reasonCode + ", message=" + message
+                + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((message == null) ? 0 : message.hashCode());
+        result = prime * result + ((reasonCode == null) ? 0 : reasonCode.hashCode());
+        result = prime * result + ((returnCode == null) ? 0 : returnCode.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        GeosupportReturnCode other = (GeosupportReturnCode) obj;
+        if (message == null) {
+            if (other.message != null)
+                return false;
+        } else if (!message.equals(other.message))
+            return false;
+        if (reasonCode == null) {
+            if (other.reasonCode != null)
+                return false;
+        } else if (!reasonCode.equals(other.reasonCode))
+            return false;
+        if (returnCode == null) {
+            if (other.returnCode != null)
+                return false;
+        } else if (!returnCode.equals(other.returnCode))
+            return false;
+        return true;
+    }
 }
--- geoclient-service/src/main/java/gov/nyc/doitt/gis/geoclient/service/search/ResponseStatus.java
+++ geoclient-service/src/main/java/gov/nyc/doitt/gis/geoclient/service/search/ResponseStatus.java
@@ -18,59 +18,91 @@ package gov.nyc.doitt.gis.geoclient.service.search;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ResponseStatus
-{
-  private GeosupportReturnCode geosupportReturnCode = new GeosupportReturnCode();
-  private GeosupportReturnCode geosupportReturnCode2 = new GeosupportReturnCode();
-  private List<String> similarNames = new ArrayList<>();
+public class ResponseStatus {
+    private GeosupportReturnCode geosupportReturnCode = new GeosupportReturnCode();
+    private GeosupportReturnCode geosupportReturnCode2 = new GeosupportReturnCode();
+    private List<String> similarNames = new ArrayList<>();
 
-  public boolean isCompassDirectionRequired()
-  {
-    return geosupportReturnCode.isCompassDirectionRequired();
-  }
+    public boolean isCompassDirectionRequired() {
+        return geosupportReturnCode.isCompassDirectionRequired();
+    }
 
-  public boolean isRejected()
-  {
-    boolean grcOneRejectedOrNull = this.geosupportReturnCode != null ? this.geosupportReturnCode.isRejected() : true;
-    boolean grcTwoRejectedOrNull = this.geosupportReturnCode2 != null ? this.geosupportReturnCode2.isRejected() : true;
-    return grcOneRejectedOrNull && grcTwoRejectedOrNull;
-  }
+    public boolean isRejected() {
+        boolean grcOneRejectedOrNull = this.geosupportReturnCode != null ? this.geosupportReturnCode.isRejected()
+                : true;
+        boolean grcTwoRejectedOrNull = this.geosupportReturnCode2 != null ? this.geosupportReturnCode2.isRejected()
+                : true;
+        return grcOneRejectedOrNull && grcTwoRejectedOrNull;
+    }
 
-  public int similarNamesCount()
-  {
-    return this.similarNames != null ? this.similarNames.size() : 0;
-  }
+    public int similarNamesCount() {
+        return this.similarNames != null ? this.similarNames.size() : 0;
+    }
 
-  public GeosupportReturnCode getGeosupportReturnCode()
-  {
-    return geosupportReturnCode;
-  }
-  public void setGeosupportReturnCode(GeosupportReturnCode geosupportReturnCode)
-  {
-    this.geosupportReturnCode = geosupportReturnCode;
-  }
-  public GeosupportReturnCode getGeosupportReturnCode2()
-  {
-    return geosupportReturnCode2;
-  }
-  public void setGeosupportReturnCode2(GeosupportReturnCode geosupportReturnCode2)
-  {
-    this.geosupportReturnCode2 = geosupportReturnCode2;
-  }
-  public List<String> getSimilarNames()
-  {
-    return similarNames;
-  }
-  public void setSimilarNames(List<String> similarNames)
-  {
-    this.similarNames = similarNames;
-  }
+    public GeosupportReturnCode getGeosupportReturnCode() {
+        return geosupportReturnCode;
+    }
 
-  @Override
-  public String toString()
-  {
-    return "ResponseStatus [rc1=" + geosupportReturnCode + ", rc2="
-        + geosupportReturnCode2 + "]";
-  }
+    public void setGeosupportReturnCode(GeosupportReturnCode geosupportReturnCode) {
+        this.geosupportReturnCode = geosupportReturnCode;
+    }
+
+    public GeosupportReturnCode getGeosupportReturnCode2() {
+        return geosupportReturnCode2;
+    }
+
+    public void setGeosupportReturnCode2(GeosupportReturnCode geosupportReturnCode2) {
+        this.geosupportReturnCode2 = geosupportReturnCode2;
+    }
+
+    public List<String> getSimilarNames() {
+        return similarNames;
+    }
+
+    public void setSimilarNames(List<String> similarNames) {
+        this.similarNames = similarNames;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseStatus [rc1=" + geosupportReturnCode + ", rc2=" + geosupportReturnCode2 + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((geosupportReturnCode == null) ? 0 : geosupportReturnCode.hashCode());
+        result = prime * result + ((geosupportReturnCode2 == null) ? 0 : geosupportReturnCode2.hashCode());
+        result = prime * result + ((similarNames == null) ? 0 : similarNames.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ResponseStatus other = (ResponseStatus) obj;
+        if (geosupportReturnCode == null) {
+            if (other.geosupportReturnCode != null)
+                return false;
+        } else if (!geosupportReturnCode.equals(other.geosupportReturnCode))
+            return false;
+        if (geosupportReturnCode2 == null) {
+            if (other.geosupportReturnCode2 != null)
+                return false;
+        } else if (!geosupportReturnCode2.equals(other.geosupportReturnCode2))
+            return false;
+        if (similarNames == null) {
+            if (other.similarNames != null)
+                return false;
+        } else if (!similarNames.equals(other.similarNames))
+            return false;
+        return true;
+    }
 
 }
