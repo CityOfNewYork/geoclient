ff3d3855dad3897ab0319518fcf1a0309f3429a0
Matthew Lipper <mlipper@doitt.nyc.gov>
2019-02-15 10:52:45 -0500

    XML reader fix. Mapper broken

--- geoclient-core/src/main/java/gov/nyc/doitt/gis/geoclient/config/GeosupportConfig.java
+++ geoclient-core/src/main/java/gov/nyc/doitt/gis/geoclient/config/GeosupportConfig.java
@@ -15,70 +15,65 @@
  */
 package gov.nyc.doitt.gis.geoclient.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import gov.nyc.doitt.gis.geoclient.config.xml.DocumentationXmlReader;
 import gov.nyc.doitt.gis.geoclient.doc.DataDictionary;
 import gov.nyc.doitt.gis.geoclient.doc.FunctionDocumentation;
 import gov.nyc.doitt.gis.geoclient.function.Function;
 import gov.nyc.doitt.gis.geoclient.jni.Geoclient;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public class GeosupportConfig {
+    private static final Logger log = LoggerFactory.getLogger(GeosupportConfig.class);
+    public static final String DEFAULT_DOCUMENTATION_CONFIG_FILE = "geoclient-doc.xml";
+    public static final String DEFAULT_CONFIG_FILE = "geoclient.xml";
+    private final Geoclient geoclient;
+    private final GeoclientXmlReader geoclientXmlReader;
+    private final DocumentationXmlReader documentationXmlReader;
 
-public class GeosupportConfig
-{
-  private static final Logger log = LoggerFactory.getLogger(GeosupportConfig.class);
-  public static final String DEFAULT_DOCUMENTATION_CONFIG_FILE = "geoclient-doc.xml";
-  public static final String DEFAULT_CONFIG_FILE = "geoclient.xml";
-  private final Geoclient geoclient;
-  private final GeoclientXmlReader geoclientXmlReader;
-  private final DocumentationXmlReader documentationXmlReader;
-
-  public GeosupportConfig(Geoclient geoclient)
-  {
-    this(DEFAULT_CONFIG_FILE, DEFAULT_DOCUMENTATION_CONFIG_FILE, geoclient);
-  }
-
-  public GeosupportConfig(String configFile, String documentationConfigFile, Geoclient geoclient)
-  {
-    this.geoclientXmlReader = GeoclientXmlReader.fromXml(configFile);
-    this.documentationXmlReader = DocumentationXmlReader.fromXml(documentationConfigFile);
-    this.geoclient = geoclient;
-    init();
-  }
-
-  public Function getFunction(String id)
-  {
-    if (!Registry.containsFunction(id))
-    {
-      throw new UnknownFunctionException(id);
+    public GeosupportConfig(Geoclient geoclient) {
+        this(DEFAULT_CONFIG_FILE, DEFAULT_DOCUMENTATION_CONFIG_FILE, geoclient);
     }
-    return Registry.getFunction(id);
-  }
 
-  public FunctionDocumentation getFunctionDocumentation(String id)
-  {
-    if (!Registry.containsFunctionDocumentation(id))
-    {
-      throw new FunctionNotDocumentedException(id);
+    public GeosupportConfig(String configFile, String documentationConfigFile, Geoclient geoclient) {
+        log.info("Loading geoclient configuration from file {}", configFile);
+        this.geoclientXmlReader = GeoclientXmlReader.fromXml(configFile);
+        log.info("Successfully loaded file {}", configFile);
+        log.info("Loading geoclient's documentation configuration from file {}", documentationConfigFile);
+        this.documentationXmlReader = DocumentationXmlReader.fromXml(documentationConfigFile);
+        log.info("Successfully loaded file {}", documentationConfigFile);
+        this.geoclient = geoclient;
+        init();
     }
-    return Registry.getFunctionDocumentation(id);
-  }
 
-  public DataDictionary getDataDictionary()
-  {
-    return this.documentationXmlReader.getDataDictionary();
-  }
-
-  private void init()
-  {
-    DocumentationConfig documentationConfig = new DocumentationConfig(documentationXmlReader.getDataDictionary());
-    for (FunctionConfig functionConfig : this.geoclientXmlReader.getFunctions())
-    {
-      log.info("Creating function from {}", functionConfig);
-      Function function = functionConfig.createFunction(geoclient);
-      Registry.addFunction(function);
-      Registry.addFunctionDocumentation(documentationConfig.document(documentationXmlReader.getFunctionDocumentation(function.getId()) , function));
+    public Function getFunction(String id) {
+        if (!Registry.containsFunction(id)) {
+            throw new UnknownFunctionException(id);
+        }
+        return Registry.getFunction(id);
     }
-  }
+
+    public FunctionDocumentation getFunctionDocumentation(String id) {
+        if (!Registry.containsFunctionDocumentation(id)) {
+            throw new FunctionNotDocumentedException(id);
+        }
+        return Registry.getFunctionDocumentation(id);
+    }
+
+    public DataDictionary getDataDictionary() {
+        return this.documentationXmlReader.getDataDictionary();
+    }
+
+    private void init() {
+        DocumentationConfig documentationConfig = new DocumentationConfig(documentationXmlReader.getDataDictionary());
+        for (FunctionConfig functionConfig : this.geoclientXmlReader.getFunctions()) {
+            log.info("Creating function from {}", functionConfig);
+            Function function = functionConfig.createFunction(geoclient);
+            Registry.addFunction(function);
+            Registry.addFunctionDocumentation(documentationConfig
+                    .document(documentationXmlReader.getFunctionDocumentation(function.getId()), function));
+        }
+    }
 
 }
--- geoclient-core/src/main/java/gov/nyc/doitt/gis/geoclient/config/xml/FieldConverter.java
+++ geoclient-core/src/main/java/gov/nyc/doitt/gis/geoclient/config/xml/FieldConverter.java
@@ -15,8 +15,6 @@
  */
 package gov.nyc.doitt.gis.geoclient.config.xml;
 
-import gov.nyc.doitt.gis.geoclient.function.Field;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,29 +24,24 @@ import com.thoughtworks.xstream.converters.UnmarshallingContext;
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
 import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 
-public class FieldConverter implements Converter
-{
-	private static final Logger log = LoggerFactory.getLogger(FieldConverter.class);
-	
-	public static class Metadata {
-	    public final String xmlFieldAttributeId;
-	    public final String xmlFieldAttributeStart;
-	    public final String xmlFieldAttributeLength;
-	    public final String xmlFieldValueCompositeType;
-	    public final String xmlFieldAttributeType;
-	    public final String xmlFieldAttributeInput;
-	    public final String xmlFieldAttributeAlias;
-	    public final String xmlFieldAttributeWhitespace;
-        public Metadata(
-                String xmlFieldAttributeId, 
-                String xmlFieldAttributeStrart,
-                String xmlFieldAttributeLength, 
-                String xmlFieldValueCompositeType,
-                String xmlFieldAttributeType, 
-                String xmlFieldAttributeInput,
-                String xmlFieldAttributeAlias, 
-                String xmlFieldAttributeWhitespace)
-        {
+import gov.nyc.doitt.gis.geoclient.function.Field;
+
+public class FieldConverter implements Converter {
+    private static final Logger log = LoggerFactory.getLogger(FieldConverter.class);
+
+    public static class Metadata {
+        public final String xmlFieldAttributeId;
+        public final String xmlFieldAttributeStart;
+        public final String xmlFieldAttributeLength;
+        public final String xmlFieldValueCompositeType;
+        public final String xmlFieldAttributeType;
+        public final String xmlFieldAttributeInput;
+        public final String xmlFieldAttributeAlias;
+        public final String xmlFieldAttributeWhitespace;
+
+        public Metadata(String xmlFieldAttributeId, String xmlFieldAttributeStrart, String xmlFieldAttributeLength,
+                String xmlFieldValueCompositeType, String xmlFieldAttributeType, String xmlFieldAttributeInput,
+                String xmlFieldAttributeAlias, String xmlFieldAttributeWhitespace) {
             super();
             this.xmlFieldAttributeId = xmlFieldAttributeId;
             this.xmlFieldAttributeStart = xmlFieldAttributeStrart;
@@ -59,42 +52,41 @@ public class FieldConverter implements Converter
             this.xmlFieldAttributeAlias = xmlFieldAttributeAlias;
             this.xmlFieldAttributeWhitespace = xmlFieldAttributeWhitespace;
         }
-	    
-	}
-	
-	private final Metadata metadata;
-	
-	public FieldConverter(Metadata metadata) {
-	    this.metadata = metadata;
-	}
 
-	@Override
-	public boolean canConvert(@SuppressWarnings("rawtypes") Class type)
-	{
-		return Field.class.isAssignableFrom(type);
-	}
+    }
 
-	@Override
-	public void marshal(Object source, HierarchicalStreamWriter writer, MarshallingContext context)
-	{
-		throw new UnsupportedOperationException("Marshalling back to XML is not implemented");
-	}
+    private final Metadata metadata;
 
-	@Override
-	public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context)
-	{
-		String id = reader.getAttribute(metadata.xmlFieldAttributeId);
-		// The start value from XML is 1-indexed and needs to be adjusted for 
-		// the Field class which expects a zero-indexed start value.
-		int start = Integer.valueOf(reader.getAttribute(metadata.xmlFieldAttributeStart)) - 1;
-		int length = Integer.valueOf(reader.getAttribute(metadata.xmlFieldAttributeLength));
-		boolean isComposite = metadata.xmlFieldValueCompositeType.equalsIgnoreCase(reader.getAttribute(metadata.xmlFieldAttributeType));
-		boolean isInput = "true".equalsIgnoreCase(reader.getAttribute(metadata.xmlFieldAttributeInput));
-		String alias = reader.getAttribute(metadata.xmlFieldAttributeAlias);
-		boolean whitespace = "true".equalsIgnoreCase(reader.getAttribute(metadata.xmlFieldAttributeWhitespace));
-		Field field = new Field(id,start,length,isComposite,isInput,alias,whitespace);
-		log.trace("Created {}", field);
-		return field;
-	}
+    public FieldConverter(Metadata metadata) {
+        this.metadata = metadata;
+    }
+
+    @Override
+    public boolean canConvert(@SuppressWarnings("rawtypes") Class type) {
+        return Field.class.isAssignableFrom(type);
+    }
+
+    @Override
+    public void marshal(Object source, HierarchicalStreamWriter writer, MarshallingContext context) {
+        throw new UnsupportedOperationException("Marshalling back to XML is not implemented");
+    }
+
+    @Override
+    public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+        String id = reader.getAttribute(metadata.xmlFieldAttributeId);
+        // The start value from XML is 1-indexed and needs to be adjusted for
+        // the Field class which expects a zero-indexed start value.
+        int start = Integer.valueOf(reader.getAttribute(metadata.xmlFieldAttributeStart)) - 1;
+        int length = Integer.valueOf(reader.getAttribute(metadata.xmlFieldAttributeLength));
+        boolean isComposite = metadata.xmlFieldValueCompositeType
+                .equalsIgnoreCase(reader.getAttribute(metadata.xmlFieldAttributeType));
+        boolean isInput = "true".equalsIgnoreCase(reader.getAttribute(metadata.xmlFieldAttributeInput));
+        String alias = reader.getAttribute(metadata.xmlFieldAttributeAlias);
+        boolean whitespace = "true".equalsIgnoreCase(reader.getAttribute(metadata.xmlFieldAttributeWhitespace));
+        Field field = new Field(id, start, length, isComposite, isInput, alias, whitespace);
+        log.info("Created {}", field);
+        // log.trace("Created {}", field);
+        return field;
+    }
 
 }
