f6ef8679934e94b264ef45b16ba8586b97d4dcc1
Matthew Lipper <mlipper@doitt.nyc.gov>
2019-02-26 15:04:37 -0500

    Adjusted regex logging to DEBUG instead of INFO

--- geoclient-parser/src/main/java/gov/nyc/doitt/gis/geoclient/parser/regex/AbstractRegexParser.java
+++ geoclient-parser/src/main/java/gov/nyc/doitt/gis/geoclient/parser/regex/AbstractRegexParser.java
@@ -15,13 +15,6 @@
  */
 package gov.nyc.doitt.gis.geoclient.parser.regex;
 
-import gov.nyc.doitt.gis.geoclient.parser.ParseContext;
-import gov.nyc.doitt.gis.geoclient.parser.Parser;
-import gov.nyc.doitt.gis.geoclient.parser.token.Chunk;
-import gov.nyc.doitt.gis.geoclient.parser.token.ChunkType;
-import gov.nyc.doitt.gis.geoclient.parser.token.Token;
-import gov.nyc.doitt.gis.geoclient.parser.token.TokenType;
-
 import java.util.List;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
@@ -29,77 +22,75 @@ import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import gov.nyc.doitt.gis.geoclient.parser.ParseContext;
+import gov.nyc.doitt.gis.geoclient.parser.Parser;
+import gov.nyc.doitt.gis.geoclient.parser.token.Chunk;
+import gov.nyc.doitt.gis.geoclient.parser.token.ChunkType;
+import gov.nyc.doitt.gis.geoclient.parser.token.Token;
+import gov.nyc.doitt.gis.geoclient.parser.token.TokenType;
 
-public abstract class AbstractRegexParser implements Parser
-{
-	private static final Logger LOGGER = LoggerFactory.getLogger(AbstractRegexParser.class);
+public abstract class AbstractRegexParser implements Parser {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractRegexParser.class);
 
-	@Override
-	public abstract void parse(ParseContext parseContext);
+    @Override
+    public abstract void parse(ParseContext parseContext);
 
-	protected void patternNotMatched(ParseContext parseContext, Pattern pattern)
-	{
-		LOGGER.info("{} could not match tokens in chunk '{}' using pattern {}",getName(),parseContext.currentChunkText(),pattern);
-	}
-	
-	protected Token newToken(TokenType type, MatchResult matchResult, int groupNumber)
-	{
-		Token token = new Token(type,matchResult.group(groupNumber),matchResult.start(groupNumber),matchResult.end(groupNumber));
-		StringBuffer buff = new StringBuffer(matchResult.group());
-		buff.insert(token.start(), '[');
-		buff.insert(token.end()+1, ']');
-		LOGGER.debug("{} recognized '{}' as a '{}' token", getName(), buff.toString(), token.getType());
-		return token;
-	}
+    protected void patternNotMatched(ParseContext parseContext, Pattern pattern) {
+        LOGGER.debug("{} could not match tokens in chunk '{}' using pattern {}", getName(),
+                parseContext.currentChunkText(), pattern);
+    }
 
-	protected void nextChunk(ParseContext parseContext, int newStart, int newEnd)
-	{
-		Chunk currentChunk = parseContext.getCurrent();
-		LOGGER.debug("Creating subChunk '{}'",currentChunk.highlight(newStart, newEnd));
-		Chunk subChunk = currentChunk.subChunk(newStart, newEnd);
-		if(!subChunk.containsText())
-		{
-			// All of the original input String has been matched for Tokens
-			parseComplete(parseContext);
-			return;
-		}
-		parseContext.setCurrent(subChunk);
-	}
-	
-	protected void handleMatch(Match match, ChunkType chunkType)
-	{
-		Chunk currentChunk = match.currentChunk();
-		currentChunk.setType(chunkType);
-		List<RegexTokenGroup> matchGroups = match.matchGroups();
-		MatchResult matchResult = match.matchResult();
-		for (RegexTokenGroup matchGroup : matchGroups)
-		{
-			int groupNumber = matchGroup.getGroup();
-			if(matchResult.group(groupNumber)!=null)
-			{
-				currentChunk.add(newToken(matchGroup.getTokenType(), matchResult, matchGroup.getGroup()));
-				LOGGER.info("{} found tokens in chunk '{}' using pattern {}",getName(),currentChunk.getText(),matchGroup.getPattern());
-				//patternMatched(match.parseContext(), matchGroup.getPattern());
-			}
-		}
-		if(MatchType.END_OF_INPUT.equals(match.getMatchType())){
-			nextChunk(match.parseContext(), 0, match.firstMatchingGroupStart());
-		}
-		if(MatchType.COMPLETE.equals(match.getMatchType()))
-		{
-			parseComplete(match.parseContext());
-		}
-	}
-	
-	private void parseComplete(ParseContext parseContext)
-	{
-		LOGGER.info("Parse complete.");
-		parseContext.setParsed(true);
-	}
+    protected Token newToken(TokenType type, MatchResult matchResult, int groupNumber) {
+        Token token = new Token(type, matchResult.group(groupNumber), matchResult.start(groupNumber),
+                matchResult.end(groupNumber));
+        StringBuffer buff = new StringBuffer(matchResult.group());
+        buff.insert(token.start(), '[');
+        buff.insert(token.end() + 1, ']');
+        LOGGER.debug("{} recognized '{}' as a '{}' token", getName(), buff.toString(), token.getType());
+        return token;
+    }
 
-	public String getName()
-	{
-		return this.getClass().getSimpleName();
-	}
+    protected void nextChunk(ParseContext parseContext, int newStart, int newEnd) {
+        Chunk currentChunk = parseContext.getCurrent();
+        LOGGER.debug("Creating subChunk '{}'", currentChunk.highlight(newStart, newEnd));
+        Chunk subChunk = currentChunk.subChunk(newStart, newEnd);
+        if (!subChunk.containsText()) {
+            // All of the original input String has been matched for Tokens
+            parseComplete(parseContext);
+            return;
+        }
+        parseContext.setCurrent(subChunk);
+    }
+
+    protected void handleMatch(Match match, ChunkType chunkType) {
+        Chunk currentChunk = match.currentChunk();
+        currentChunk.setType(chunkType);
+        List<RegexTokenGroup> matchGroups = match.matchGroups();
+        MatchResult matchResult = match.matchResult();
+        for (RegexTokenGroup matchGroup : matchGroups) {
+            int groupNumber = matchGroup.getGroup();
+            if (matchResult.group(groupNumber) != null) {
+                currentChunk.add(newToken(matchGroup.getTokenType(), matchResult, matchGroup.getGroup()));
+                LOGGER.debug("{} found tokens in chunk '{}' using pattern {}", getName(), currentChunk.getText(),
+                        matchGroup.getPattern());
+                // patternMatched(match.parseContext(), matchGroup.getPattern());
+            }
+        }
+        if (MatchType.END_OF_INPUT.equals(match.getMatchType())) {
+            nextChunk(match.parseContext(), 0, match.firstMatchingGroupStart());
+        }
+        if (MatchType.COMPLETE.equals(match.getMatchType())) {
+            parseComplete(match.parseContext());
+        }
+    }
+
+    private void parseComplete(ParseContext parseContext) {
+        LOGGER.debug("Parse complete.");
+        parseContext.setParsed(true);
+    }
+
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
 
 }
