24b2f7b4d05ceb03d4a89ef3ef24eb82863529e2
Matthew Lipper <mlipper@doitt.nyc.gov>
2019-06-21 13:51:19 -0400

    'bootWar' task builds stand-alone & deployable war
    By inheriting from SpringBootServletInitializer the GeoclientBootApplication
    class allows Gradle to build a war file that can be run standalone or
    deployed to a Servlet 3.0+ container.
    
    This functionality is provided by the 'bootWar' implemented by
    Spring Boot Gradle plugin.
    
    Note that this commit disables the default Gradle jar and war tasks.
    Currently, these still show up when running the 'tasks' task. Worse,
    unless log levels are increased, there is no indication that these tasks
    are skipped due to an 'onlyIf' qualifier injected by the Spring Boot
    Gradle Plugin.

--- geoclient-service/src/main/java/gov/nyc/doitt/gis/geoclient/service/GeoclientBootApplication.java
+++ geoclient-service/src/main/java/gov/nyc/doitt/gis/geoclient/service/GeoclientBootApplication.java
@@ -2,9 +2,16 @@ package gov.nyc.doitt.gis.geoclient.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GeoclientBootApplication {
+public class GeoclientBootApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GeoclientBootApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeoclientBootApplication.class, args);
