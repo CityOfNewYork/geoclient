ed7a7719558850f89b2f06d798a6669e8d38f798
Matthew Lipper <mlipper@gmail.com>
2019-11-06 17:41:53 -0500

    Add two additional street codes to street endpoint
    Add additional streetCodeTwo and streetCodeThree to 'street' endpoint to
    match actual Geosupport function D* APIs.
    
    Reformat file to replace tabs with spaces.

--- geoclient-service/src/main/java/gov/nyc/doitt/gis/geoclient/service/web/RestController.java
+++ geoclient-service/src/main/java/gov/nyc/doitt/gis/geoclient/service/web/RestController.java
@@ -50,202 +50,205 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class RestController
 {
 
-	public static final String ADDRESS_URI = "/address";
-	public static final String ADDRESSPOINT_URI = "/addresspoint";
-	public static final String BBL_URI = "/bbl";
-	public static final String BIN_URI = "/bin";
-	public static final String BLOCKFACE_URI = "/blockface";
-	public static final String DOC_URI = "/doc";
-	public static final String GEOSUPPORT_URI = "/geosupport";
-	public static final String INTERSECTION_URI = "/intersection";
-	public static final String NORMALIZE_URI= "/normalize";
+    public static final String ADDRESS_URI = "/address";
+    public static final String ADDRESSPOINT_URI = "/addresspoint";
+    public static final String BBL_URI = "/bbl";
+    public static final String BIN_URI = "/bin";
+    public static final String BLOCKFACE_URI = "/blockface";
+    public static final String DOC_URI = "/doc";
+    public static final String GEOSUPPORT_URI = "/geosupport";
+    public static final String INTERSECTION_URI = "/intersection";
+    public static final String NORMALIZE_URI= "/normalize";
     public static final String PLACE_URI = "/place";
     public static final String STREET_URI = "/street";
-	public static final String VERSION_URI = "/version";
+    public static final String VERSION_URI = "/version";
 
-	public static final String ADDRESS_OBJ = ServiceType.ADDRESS.elementName();
-	public static final String ADDRESSPOINT_OBJ = ServiceType.ADDRESSPOINT.elementName();
-	public static final String BBL_OBJ = ServiceType.BBL.elementName();
-	public static final String BIN_OBJ = ServiceType.BIN.elementName();
-	public static final String BLOCKFACE_OBJ = ServiceType.BLOCKFACE.elementName();
-	public static final String DOC_OBJ = ServiceType.DOC.elementName();
-	public static final String INTERSECTION_OBJ = ServiceType.INTERSECTION.elementName();
-	public static final String NORMALIZE_OBJ = ServiceType.NORMALIZE.elementName();
-	public static final String PLACE_OBJ = ServiceType.PLACE.elementName();
+    public static final String ADDRESS_OBJ = ServiceType.ADDRESS.elementName();
+    public static final String ADDRESSPOINT_OBJ = ServiceType.ADDRESSPOINT.elementName();
+    public static final String BBL_OBJ = ServiceType.BBL.elementName();
+    public static final String BIN_OBJ = ServiceType.BIN.elementName();
+    public static final String BLOCKFACE_OBJ = ServiceType.BLOCKFACE.elementName();
+    public static final String DOC_OBJ = ServiceType.DOC.elementName();
+    public static final String INTERSECTION_OBJ = ServiceType.INTERSECTION.elementName();
+    public static final String NORMALIZE_OBJ = ServiceType.NORMALIZE.elementName();
+    public static final String PLACE_OBJ = ServiceType.PLACE.elementName();
     public static final String STREET_OBJ = ServiceType.STREET.elementName();
     public static final String VERSION_OBJ = ServiceType.VERSION.elementName();
 
-	//public static final String DEFAULT_STREET_NAME_FORMAT = StreetNameFormat.SORT.elementName();
+    //public static final String DEFAULT_STREET_NAME_FORMAT = StreetNameFormat.SORT.elementName();
 
-	public static final String DOC_VIEW_NAME = "index";
+    public static final String DOC_VIEW_NAME = "index";
 
-	private static final Logger logger = LoggerFactory.getLogger(RestController.class);
+    private static final Logger logger = LoggerFactory.getLogger(RestController.class);
 
-	@Autowired
-	private GeosupportService geosupportService;
+    @Autowired
+    private GeosupportService geosupportService;
 
-	@RequestMapping(value = ADDRESS_URI, method = RequestMethod.GET)
-	public @ResponseBody Map<String, Object> address(
-		    @RequestParam(required = false) String houseNumber,
-			@RequestParam String street,
-			@RequestParam(required = false) String borough,
-			@RequestParam(required = false) String zip) throws Exception
-	{
-		logger.debug("address[houseNumber='{}', street='{}', borough='{}', zip='{}']", houseNumber, street, borough, zip);
-		if (borough == null && zip == null)
-		{
-			throw new MissingAnyOfOptionalServletRequestParametersException("borough", "zip");
-		}
-		Map<String, Object> addressMap = new HashMap<String, Object>();
-		addressMap.put(ADDRESS_OBJ, this.geosupportService.callFunction1B(houseNumber, street, borough, zip));
-		return addressMap;
-	}
+    @RequestMapping(value = ADDRESS_URI, method = RequestMethod.GET)
+    public @ResponseBody Map<String, Object> address(
+            @RequestParam(required = false) String houseNumber,
+            @RequestParam String street,
+            @RequestParam(required = false) String borough,
+            @RequestParam(required = false) String zip) throws Exception
+    {
+        logger.debug("address[houseNumber='{}', street='{}', borough='{}', zip='{}']", houseNumber, street, borough, zip);
+        if (borough == null && zip == null)
+        {
+            throw new MissingAnyOfOptionalServletRequestParametersException("borough", "zip");
+        }
+        Map<String, Object> addressMap = new HashMap<String, Object>();
+        addressMap.put(ADDRESS_OBJ, this.geosupportService.callFunction1B(houseNumber, street, borough, zip));
+        return addressMap;
+    }
 
-	@RequestMapping(value = ADDRESSPOINT_URI, method = RequestMethod.GET)
-	public @ResponseBody Map<String, Object> addresspoint(
-		    @RequestParam(required = false) String houseNumber,
-			@RequestParam String street,
-			@RequestParam(required = false) String borough,
-			@RequestParam(required = false) String zip) throws Exception
-	{
-		logger.debug("addresspoint[houseNumber='{}', street='{}', borough='{}', zip='{}']", houseNumber, street, borough, zip);
-		if (borough == null && zip == null)
-		{
-			throw new MissingAnyOfOptionalServletRequestParametersException("borough", "zip");
-		}
-		Map<String, Object> addressPointMap = new HashMap<String, Object>();
-		addressPointMap.put(ADDRESSPOINT_OBJ, this.geosupportService.callFunctionAP(houseNumber, street, borough, zip));
-		return addressPointMap;
-	}
+    @RequestMapping(value = ADDRESSPOINT_URI, method = RequestMethod.GET)
+    public @ResponseBody Map<String, Object> addresspoint(
+            @RequestParam(required = false) String houseNumber,
+            @RequestParam String street,
+            @RequestParam(required = false) String borough,
+            @RequestParam(required = false) String zip) throws Exception
+    {
+        logger.debug("addresspoint[houseNumber='{}', street='{}', borough='{}', zip='{}']", houseNumber, street, borough, zip);
+        if (borough == null && zip == null)
+        {
+            throw new MissingAnyOfOptionalServletRequestParametersException("borough", "zip");
+        }
+        Map<String, Object> addressPointMap = new HashMap<String, Object>();
+        addressPointMap.put(ADDRESSPOINT_OBJ, this.geosupportService.callFunctionAP(houseNumber, street, borough, zip));
+        return addressPointMap;
+    }
 
-	@RequestMapping(value = PLACE_URI, method = RequestMethod.GET)
-	public @ResponseBody
-	Map<String, Object> place(
-			@RequestParam String name,
-			@RequestParam(required = false) String borough,
-			@RequestParam(required = false) String zip) throws Exception
-	{
-		logger.debug("place[name='{}', borough='{}', zip='{}']", name, borough,zip);
-		if (borough == null && zip == null)
-		{
-			throw new MissingAnyOfOptionalServletRequestParametersException("borough", "zip");
-		}
-		Map<String, Object> placeMap = new HashMap<String, Object>();
-		placeMap.put(PLACE_OBJ, this.geosupportService.callFunction1B(null, name, borough, zip));
-		return placeMap;
-	}
+    @RequestMapping(value = PLACE_URI, method = RequestMethod.GET)
+    public @ResponseBody
+    Map<String, Object> place(
+            @RequestParam String name,
+            @RequestParam(required = false) String borough,
+            @RequestParam(required = false) String zip) throws Exception
+    {
+        logger.debug("place[name='{}', borough='{}', zip='{}']", name, borough,zip);
+        if (borough == null && zip == null)
+        {
+            throw new MissingAnyOfOptionalServletRequestParametersException("borough", "zip");
+        }
+        Map<String, Object> placeMap = new HashMap<String, Object>();
+        placeMap.put(PLACE_OBJ, this.geosupportService.callFunction1B(null, name, borough, zip));
+        return placeMap;
+    }
 
-	@RequestMapping(value = INTERSECTION_URI, method = RequestMethod.GET)
-	public @ResponseBody
-	Map<String, Object> intersection(@RequestParam String crossStreetOne, @RequestParam String crossStreetTwo,
-			@RequestParam String borough, @RequestParam(required = false) String boroughCrossStreetTwo,
-			@RequestParam(required = false) String compassDirection)
-	{
-		logger.debug(
-				"intersection[crossStreetOne='{}', crossStreetTwo='{}', borough='{}', boroughCrossStreetTwo='{}', compassDirection='{}']",
-				crossStreetOne, crossStreetTwo, borough, boroughCrossStreetTwo, compassDirection);
-		Map<String, Object> intersectionMap = new HashMap<String, Object>();
-		intersectionMap.put(INTERSECTION_OBJ, this.geosupportService.callFunction2(crossStreetOne, borough,
-				crossStreetTwo, boroughCrossStreetTwo, compassDirection));
-		return intersectionMap;
-	}
+    @RequestMapping(value = INTERSECTION_URI, method = RequestMethod.GET)
+    public @ResponseBody
+    Map<String, Object> intersection(@RequestParam String crossStreetOne, @RequestParam String crossStreetTwo,
+            @RequestParam String borough, @RequestParam(required = false) String boroughCrossStreetTwo,
+            @RequestParam(required = false) String compassDirection)
+    {
+        logger.debug(
+                "intersection[crossStreetOne='{}', crossStreetTwo='{}', borough='{}', boroughCrossStreetTwo='{}', compassDirection='{}']",
+                crossStreetOne, crossStreetTwo, borough, boroughCrossStreetTwo, compassDirection);
+        Map<String, Object> intersectionMap = new HashMap<String, Object>();
+        intersectionMap.put(INTERSECTION_OBJ, this.geosupportService.callFunction2(crossStreetOne, borough,
+                crossStreetTwo, boroughCrossStreetTwo, compassDirection));
+        return intersectionMap;
+    }
 
-	@RequestMapping(value = BLOCKFACE_URI, method = RequestMethod.GET)
-	public @ResponseBody
-	Map<String, Object> blockface(@RequestParam String onStreet, @RequestParam String crossStreetOne,
-			@RequestParam String crossStreetTwo, @RequestParam String borough,
-			@RequestParam(required = false) String boroughCrossStreetOne,
-			@RequestParam(required = false) String boroughCrossStreetTwo,
-			@RequestParam(required = false) String compassDirection)
-	{
-		logger.debug(
-				"blockface[onStreet='{}',crossStreetOne='{}', crossStreetTwo='{}', borough='{}', boroughCrossStreetOne='{}', boroughCrossStreetTwo='{}', compassDirection='{}']",
-				onStreet, crossStreetOne, crossStreetTwo, borough, boroughCrossStreetOne, boroughCrossStreetTwo,
-				compassDirection);
-		Map<String, Object> blockfaceMap = new HashMap<String, Object>();
-		blockfaceMap.put(BLOCKFACE_OBJ, this.geosupportService.callFunction3(onStreet, borough, crossStreetOne,
-				boroughCrossStreetOne, crossStreetTwo, boroughCrossStreetTwo, compassDirection));
-		return blockfaceMap;
-	}
+    @RequestMapping(value = BLOCKFACE_URI, method = RequestMethod.GET)
+    public @ResponseBody
+    Map<String, Object> blockface(@RequestParam String onStreet, @RequestParam String crossStreetOne,
+            @RequestParam String crossStreetTwo, @RequestParam String borough,
+            @RequestParam(required = false) String boroughCrossStreetOne,
+            @RequestParam(required = false) String boroughCrossStreetTwo,
+            @RequestParam(required = false) String compassDirection)
+    {
+        logger.debug(
+                "blockface[onStreet='{}',crossStreetOne='{}', crossStreetTwo='{}', borough='{}', boroughCrossStreetOne='{}', boroughCrossStreetTwo='{}', compassDirection='{}']",
+                onStreet, crossStreetOne, crossStreetTwo, borough, boroughCrossStreetOne, boroughCrossStreetTwo,
+                compassDirection);
+        Map<String, Object> blockfaceMap = new HashMap<String, Object>();
+        blockfaceMap.put(BLOCKFACE_OBJ, this.geosupportService.callFunction3(onStreet, borough, crossStreetOne,
+                boroughCrossStreetOne, crossStreetTwo, boroughCrossStreetTwo, compassDirection));
+        return blockfaceMap;
+    }
 
-	@RequestMapping(value = BBL_URI, method = RequestMethod.GET)
-	public @ResponseBody
-	Map<String, Object> bbl(@RequestParam String borough, @RequestParam String block, @RequestParam String lot)
-	{
-		logger.debug("bbl[borough='{}',block='{}', lot='{}']", borough, block, lot);
-		Map<String, Object> bblMap = new HashMap<String, Object>();
-		bblMap.put(BBL_OBJ, this.geosupportService.callFunctionBL(borough, block, lot));
-		return bblMap;
-	}
+    @RequestMapping(value = BBL_URI, method = RequestMethod.GET)
+    public @ResponseBody
+    Map<String, Object> bbl(@RequestParam String borough, @RequestParam String block, @RequestParam String lot)
+    {
+        logger.debug("bbl[borough='{}',block='{}', lot='{}']", borough, block, lot);
+        Map<String, Object> bblMap = new HashMap<String, Object>();
+        bblMap.put(BBL_OBJ, this.geosupportService.callFunctionBL(borough, block, lot));
+        return bblMap;
+    }
 
-	@RequestMapping(value = BIN_URI, method = RequestMethod.GET)
-	public @ResponseBody
-	Map<String, Object> bin(@RequestParam String bin)
-	{
-		logger.debug("bin[bin='{}']", bin);
-		Map<String, Object> binMap = new HashMap<String, Object>();
-		binMap.put(BIN_OBJ, this.geosupportService.callFunctionBN(bin));
-		return binMap;
-	}
+    @RequestMapping(value = BIN_URI, method = RequestMethod.GET)
+    public @ResponseBody
+    Map<String, Object> bin(@RequestParam String bin)
+    {
+        logger.debug("bin[bin='{}']", bin);
+        Map<String, Object> binMap = new HashMap<String, Object>();
+        binMap.put(BIN_OBJ, this.geosupportService.callFunctionBN(bin));
+        return binMap;
+    }
 
-	@RequestMapping(value = NORMALIZE_URI, method = RequestMethod.GET)
-	public @ResponseBody
-	Map<String, Object> normalize(@RequestParam String name, @RequestParam(required = false, defaultValue = "32") Integer length, @RequestParam(required = false, defaultValue = "S") String format)
-	{
-		logger.debug("normalize[name='{}',length='{}',format='{}']", name, length, format);
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put(NORMALIZE_OBJ, this.geosupportService.callFunctionN(name, length, format));
-		return resultMap;
-	}
-
-    @RequestMapping(value = STREET_URI, method = RequestMethod.GET)
-    public @ResponseBody Map<String, Object> street(@RequestParam String streetCode,
-            @RequestParam(required = false, defaultValue = "32") Integer length,
-            @RequestParam(required = false, defaultValue = "S") String format) {
-        logger.debug("street[streetCode='{}',length='{}',format='{}']", streetCode, length, format);
+    @RequestMapping(value = NORMALIZE_URI, method = RequestMethod.GET)
+    public @ResponseBody
+    Map<String, Object> normalize(@RequestParam String name, @RequestParam(required = false, defaultValue = "32") Integer length, @RequestParam(required = false, defaultValue = "S") String format)
+    {
+        logger.debug("normalize[name='{}',length='{}',format='{}']", name, length, format);
         Map<String, Object> resultMap = new HashMap<String, Object>();
-        resultMap.put(STREET_OBJ, this.geosupportService.callFunctionD(streetCode, length, format));
+        resultMap.put(NORMALIZE_OBJ, this.geosupportService.callFunctionN(name, length, format));
         return resultMap;
     }
 
-	@RequestMapping(value = GEOSUPPORT_URI, method = RequestMethod.GET)
-	public @ResponseBody
-	Map<String, Object> geosupport(@RequestParam Map<String, Object> params)
-	{
-		logger.debug("geosupport[{}]", params);
-		return this.geosupportService.callGeosupport(params);
-	}
+    @RequestMapping(value = STREET_URI, method = RequestMethod.GET)
+    public @ResponseBody Map<String, Object> street(@RequestParam String streetCode,
+            @RequestParam(required = false) String streetCodeTwo,
+            @RequestParam(required = false) String streetCodeThree,
+            @RequestParam(required = false, defaultValue = "32") Integer length,
+            @RequestParam(required = false, defaultValue = "S") String format) {
+        logger.debug("street[streetCode='{}',streetCodeTwo='{}',streetCodeThree='{}',length='{}',format='{}']",
+                streetCode, streetCodeTwo, streetCodeThree, length, format);
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put(STREET_OBJ, this.geosupportService.callFunctionD(streetCode, streetCodeTwo, streetCodeThree, length, format));
+        return resultMap;
+    }
 
-	@RequestMapping(value = DOC_URI, method = RequestMethod.GET)
-	public String doc(ModelMap modelMap)
-	{
-		modelMap.put(DOC_OBJ, this.geosupportService.getDocumentation());
-		modelMap.put(VERSION_OBJ, this.geosupportService.version());
-		return DOC_VIEW_NAME;
-	}
+    @RequestMapping(value = GEOSUPPORT_URI, method = RequestMethod.GET)
+    public @ResponseBody
+    Map<String, Object> geosupport(@RequestParam Map<String, Object> params)
+    {
+        logger.debug("geosupport[{}]", params);
+        return this.geosupportService.callGeosupport(params);
+    }
 
-	@RequestMapping(value = VERSION_URI, method = RequestMethod.GET)
-	public @ResponseBody
-	Version version()
-	{
-		return this.geosupportService.version();
-	}
+    @RequestMapping(value = DOC_URI, method = RequestMethod.GET)
+    public String doc(ModelMap modelMap)
+    {
+        modelMap.put(DOC_OBJ, this.geosupportService.getDocumentation());
+        modelMap.put(VERSION_OBJ, this.geosupportService.version());
+        return DOC_VIEW_NAME;
+    }
 
-	@ExceptionHandler(value = { MissingAnyOfOptionalServletRequestParametersException.class,
-			MissingServletRequestParameterException.class })
-	public @ResponseBody
-	ResponseEntity<BadRequest> handleMissingRequestParameter(ServletRequestBindingException exception,
-			HttpServletRequest req)
-	{
-		BadRequest badRequest = new BadRequest();
-		badRequest.setHttpStatus(HttpStatus.BAD_REQUEST.toString());
-		badRequest.setMessage(exception.getMessage());
-		badRequest.setRequestUri(String.format("%s?%s", req.getRequestURI(), req.getQueryString()));
-		return new ResponseEntity<BadRequest>(badRequest, HttpStatus.BAD_REQUEST);
-	}
+    @RequestMapping(value = VERSION_URI, method = RequestMethod.GET)
+    public @ResponseBody
+    Version version()
+    {
+        return this.geosupportService.version();
+    }
 
-	public void setGeosupportService(GeosupportService geosupportService)
-	{
-		this.geosupportService = geosupportService;
-	}
+    @ExceptionHandler(value = { MissingAnyOfOptionalServletRequestParametersException.class,
+            MissingServletRequestParameterException.class })
+    public @ResponseBody
+    ResponseEntity<BadRequest> handleMissingRequestParameter(ServletRequestBindingException exception,
+            HttpServletRequest req)
+    {
+        BadRequest badRequest = new BadRequest();
+        badRequest.setHttpStatus(HttpStatus.BAD_REQUEST.toString());
+        badRequest.setMessage(exception.getMessage());
+        badRequest.setRequestUri(String.format("%s?%s", req.getRequestURI(), req.getQueryString()));
+        return new ResponseEntity<BadRequest>(badRequest, HttpStatus.BAD_REQUEST);
+    }
+
+    public void setGeosupportService(GeosupportService geosupportService)
+    {
+        this.geosupportService = geosupportService;
+    }
 }
