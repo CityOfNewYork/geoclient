7882e1df563372c1590867fa9baafb463541b4f5
Matthew Lipper <mlipper@gmail.com>
2019-11-25 18:24:55 -0500

    Fix for geosupport.version and DG integ test

--- geoclient-service/src/integrationTest/java/gov/nyc/doitt/gis/geoclient/service/web/RestControllerIntegrationTest.java
+++ geoclient-service/src/integrationTest/java/gov/nyc/doitt/gis/geoclient/service/web/RestControllerIntegrationTest.java
@@ -1,6 +1,13 @@
 package gov.nyc.doitt.gis.geoclient.service.web;
 
+import static gov.nyc.doitt.gis.geoclient.api.InputParam.STREET_CODE;
+import static gov.nyc.doitt.gis.geoclient.api.OutputParam.GEOSUPPORT_RETURN_CODE;
+import static gov.nyc.doitt.gis.geoclient.api.ReturnCodeValue.SUCCESS;
+import static gov.nyc.doitt.gis.geoclient.function.Function.FDG;
+import static gov.nyc.doitt.gis.geoclient.service.web.RestController.STREETCODE_URI;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,6 +29,20 @@ public class RestControllerIntegrationTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testStreetcode_FDG() {
+    	String streetCodeUriTemplate = String.format("%s.%s?%s={%s}", STREETCODE_URI, "json", STREET_CODE, STREET_CODE);
+    	LOGGER.info("URI=" + streetCodeUriTemplate);
+		Map<String, Object> body = (Map<String, Object>) this.restTemplate.getForObject(streetCodeUriTemplate, Map.class, "110610");
+        LOGGER.info("/streetcode response body: {}",body);
+        Map<String, Object> result = (Map<String, Object>) body.get(STREET_CODE.toLowerCase());
+        assertThat(result.containsKey(GEOSUPPORT_RETURN_CODE));
+        assertThat(result.get("geosupportReturnCode").equals(SUCCESS));
+        assertThat(result.containsKey("geosupportFunctionCode"));
+        assertThat(result.get("geosupportFunctionCode").equals(FDG));
+    }
+    
     @Test
     public void testDoc() {
         String body = this.restTemplate.getForObject("/doc", String.class);
