ad8d880589bbcd1621262b1d8813b6ef87960735
Matthew Lipper <mlipper@gmail.com>
2019-12-31 21:46:51 -0500

    Initial pass at removing Spring dependencies
    Replace the use of org.springframework.util.Assert with
    gov.nyc.doitt.gis.geoclient.parser.util.Assert. Implemented by cut and
    paste from the Spring class and use of Apache Commons Lang3 library
    preserving existing semantics.
    
    This project still depends on Spring's dependency injection and the next
    pass will be to replace this with javax.inject.

--- geoclient-parser/src/main/java/gov/nyc/doitt/gis/geoclient/parser/Input.java
+++ geoclient-parser/src/main/java/gov/nyc/doitt/gis/geoclient/parser/Input.java
@@ -16,8 +16,8 @@
 package gov.nyc.doitt.gis.geoclient.parser;
 
 import gov.nyc.doitt.gis.geoclient.parser.token.TextUtils;
+import gov.nyc.doitt.gis.geoclient.parser.util.Assert;
 
-import org.springframework.util.Assert;
 
 /**
  * @author mlipper
