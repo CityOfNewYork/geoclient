7a1e8b79ab7d823265e84eac15f5771ff2797d72
Matthew Lipper <mlipper@gmail.com>
2022-03-11 20:48:00 +0000

    Remove Kuberetes artifacts

--- watch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# watch the java files and continously deploy the service
-gradle build
-skaffold run -p dev
-reflex -r "\.java$" -- bash -c 'gradle build && skaffold run -p dev'
