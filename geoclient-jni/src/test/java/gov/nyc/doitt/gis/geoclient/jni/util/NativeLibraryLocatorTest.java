d51b752ebcb2e0cc9a8e3c74e9d1ce8169756461
Matthew Lipper <mlipper@gmail.com>
2023-06-01 12:16:58 -0400

    Add empty line to imports per spotless policy.

--- geoclient-jni/src/test/java/gov/nyc/doitt/gis/geoclient/jni/util/NativeLibraryLocatorTest.java
+++ geoclient-jni/src/test/java/gov/nyc/doitt/gis/geoclient/jni/util/NativeLibraryLocatorTest.java
@@ -28,7 +28,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
