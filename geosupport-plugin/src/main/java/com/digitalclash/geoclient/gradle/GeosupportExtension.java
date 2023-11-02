d39a7b916726e60b6e34636e9a06fefbbb6d7b96
Matthew Lipper <mlipper@gmail.com>
2023-11-02 19:12:19 +0000

    API documentation.

--- geosupport-plugin/src/main/java/com/digitalclash/geoclient/gradle/GeosupportExtension.java
+++ geosupport-plugin/src/main/java/com/digitalclash/geoclient/gradle/GeosupportExtension.java
@@ -27,7 +27,23 @@ import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputDirectory;
 import org.gradle.api.tasks.Optional;
+import org.gradle.api.tasks.PathSensitive;
+import org.gradle.api.tasks.PathSensitivity;
 
+/**
+ * Gradle plugin extension for setting or getting Geosupport installation properties.
+ * All properties not set by the build use conventional defaults.
+ * <p>
+ * The following example shows how to use this extension in a build script with the Groovy DSL:
+ * <pre>
+ * geosupport {
+ *     home = '/opt/geosupport/current'
+ *     geofiles = '/opt/geosupport/current/fls/'
+ *     libraryPath = '/opt/geosupport/current/lib'
+ *     includePath = fileTree(dir: 'src/main/headers')
+ * }
+ * </pre>
+ */
 public class GeosupportExtension {
 
     private final Property<String> geofiles;
@@ -35,27 +51,79 @@ public class GeosupportExtension {
     private final DirectoryProperty includePath;
     private final Property<String> libraryPath;
 
-    /*
-     * TODO Use of org.gradle.api.tasks.PathSensitive and org.gradle.api.tasks.PathSensitivity?
+    /**
+     * Value to use for required Geosupport runtime enviroment
+     * variable, <code>GEOFILES</code>. This should be the absolute path
+     * to the Geosupport data files directory, typically:
+     * <code>$GEOSUPPORT_HOME/fls/</code>.
+     * <p>
+     * <strong>IMPORTANT</strong>: Geosupport <em>requires</em> that this
+     * path end with a trailing slash (<code>java.file.separator</code>).
+     * <p>
+     * Defaults to:
+     * <ul>
+     * <li><code>/opt/geosupport/current/fls/</code> on Linux/Unix</li>
+     * <li><code>c:/opt/geosupport/current/fls/</code> on Windows</li>
+     * </ul>
      */
-
     @Input
     public final Property<String> getGeofiles() {
         return geofiles;
     }
 
+    /**
+     * Value to use for the optional <code>GEOSUPPORT_HOME</code> enviroment
+     * variable. This variable is not used directly by Geosupport but is
+     * often useful for configuring client application runtimes and deployments.
+     * <p>
+     * Defaults to:
+     * <ul>
+     * <li><code>/opt/geosupport/current</code> on Linux/Unix</li>
+     * <li><code>c:/opt/geosupport/current</code> on Windows</li>
+     * </ul>
+     */
     @Input
     @Optional
     public final Property<String> getHome() {
         return home;
     }
 
+    /**
+     * The path to the directory containing Geosupport's C header files.
+     * This property is not used directly by Geosupport but is
+     * often useful for compiling C/C++ or JNI code that uses the Geosupport
+     * C API.
+     * <p>
+     * NOTE: Unlike the other extension
+     * <code>org.gradle.api.provider.Property<String></code> properties, this
+     * property is a <code>org.gradle.api.file.DirectoryProperty<String></code>
+     * to make it easier to integrate in a Gradle build.
+     * <p>
+     * Defaults to:
+     * <ul>
+     * <li><code>/opt/geosupport/current/include</code> on Linux/Unix</li>
+     * <li><code>c:/opt/geosupport/current/include</code> on Windows</li>
+     * </ul>
+     */
     @InputDirectory
     @Optional
+    @PathSensitive(PathSensitivity.RELATIVE)
     public final DirectoryProperty getIncludePath() {
         return includePath;
     }
 
+    /**
+     * The path to the directory containing Geosupport's C shared libraries.
+     * This property is not used directly by Geosupport but is
+     * often useful for configuring client application runtimes for things
+     * like <code>LD_LIBRARY_PATH</code> and <code>java.library.path</code>.
+     * <p>
+     * Defaults to:
+     * <ul>
+     * <li><code>/opt/geosupport/current/lib</code> on Linux/Unix</li>
+     * <li><code>c:/opt/geosupport/current/lib</code> on Windows</li>
+     * </ul>
+     */
     @Input
     @Optional
     public final Property<String> getLibraryPath() {
