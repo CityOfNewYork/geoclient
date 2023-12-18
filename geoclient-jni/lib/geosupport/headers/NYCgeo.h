95457e7c02e640796356086997dc18ddaf2d0b5b
Matthew Lipper <mlipper@gmail.com>
2023-12-18 15:26:46 +0000

    Replace tabs with spaces.

--- geoclient-jni/lib/geosupport/headers/NYCgeo.h
+++ geoclient-jni/lib/geosupport/headers/NYCgeo.h
@@ -3,13 +3,13 @@
 #define NYCgeoH
 
 #ifdef __cplusplus
-extern "C" 
+extern "C"
 {
-	__declspec(dllimport) void __stdcall NYCgeo(char *ptr_wa1, char *ptr_wa2=NULL);
+    __declspec(dllimport) void __stdcall NYCgeo(char *ptr_wa1, char *ptr_wa2=NULL);
 }
 #else
 {
-	__declspec(dllimport) void __stdcall NYCgeo(char *ptr_wa1, char *ptr_wa2);
+    __declspec(dllimport) void __stdcall NYCgeo(char *ptr_wa1, char *ptr_wa2);
 }
 #endif
 #endif
