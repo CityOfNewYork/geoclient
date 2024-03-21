0f1516c6afcca2e1cae4d08133e71c5484b7be81
Matthew Lipper <mlipper@gmail.com>
2024-03-21 18:29:47 +0000

    Fix usage of Spring URI builders APIs.

--- documentation/src/integrationTest/java/gov/nyc/doitt/gis/geoclient/docs/ServiceClientIntegrationTests.java
+++ documentation/src/integrationTest/java/gov/nyc/doitt/gis/geoclient/docs/ServiceClientIntegrationTests.java
@@ -15,21 +15,77 @@
  */
 package gov.nyc.doitt.gis.geoclient.docs;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableConfigurationProperties(ExternalProperties.class)
 public class ServiceClientIntegrationTests {
+
+	private static final Pattern URI_WITHOUT_QUERY_PATTERN = Pattern.compile("([^?]+)(.*)");
+	private static final Pattern QUERY_PARAM_PATTERN = Pattern.compile("([^&=]+)(=?)([^&]+)?");
 
     @Autowired
     private ServiceClient client;
+    @Autowired
+    private ExternalProperties props;
 
-    @Test
-    public void testServiceClient() {
-        assertNotNull(client);
+    @BeforeEach
+    public void setUp() {
+        assertNotNull(this.client);
     }
 
+    @Test
+    public void testBuildUri_search() {
+        Sample sample = new Sample.Builder("1", PathVariable.SEARCH, "search test").withQueryString("input", "120 Broadway").build();
+        URI uri = this.client.buildUri(sample);
+        assertEquals(props.getBaseUrl() + "/search?input=120%20Broadway", uri.toString());
+    }
+
+    @Test
+    public void testBuildUri_address() throws Exception {
+        Sample sample = new Sample.Builder("1", PathVariable.ADDRESS, "search test").withQueryString("houseNumber", "120", "street", "Broadway", "borough", "Manhattan").build();
+        Map<String,String> expectedQueryMap = sample.getQueryString();
+        URI uri = this.client.buildUri(sample);
+        Map<String, String> actualQueryMap = queryStringToMap(uri.getQuery());
+        assertEquals(expectedQueryMap, actualQueryMap);
+        String actualUriWithoutQueryString = uriWithoutQueryString(uri.toString());
+        assertEquals(props.getBaseUrl() + "/address", actualUriWithoutQueryString);
+    }
+
+    private Map<String, String> queryStringToMap(String query) {
+        Map<String, String> result = new HashMap<>();
+		if (query != null) {
+			Matcher matcher = QUERY_PARAM_PATTERN.matcher(query);
+			while (matcher.find()) {
+				String name = matcher.group(1);
+				String value = matcher.group(3);
+                result.put(name, value);
+			}
+		}
+        return result;
+    }
+
+    private String uriWithoutQueryString(String uriString) {
+        String result = null;
+		if (uriString != null) {
+			Matcher matcher = URI_WITHOUT_QUERY_PATTERN.matcher(uriString);
+			while (matcher.find()) {
+				result = matcher.group(1);
+			}
+		}
+        return result;
+    }
 }
