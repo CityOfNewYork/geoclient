0484c15cd1739c1af5079dde92b5b7dba5a87365
Matthew Lipper <mlipper@gmail.com>
2024-08-30 20:18:36 +0000

    GenerateSamplesTask writes .jsonc files.

--- buildSrc/src/main/java/geoclientbuild/docs/GenerateSamplesTask.java
+++ buildSrc/src/main/java/geoclientbuild/docs/GenerateSamplesTask.java
@@ -3,12 +3,15 @@ package geoclientbuild.docs;
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
-
-//import javax.inject.Inject;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.DirectoryProperty;
+import org.gradle.api.file.RegularFile;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
@@ -24,6 +27,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 abstract public class GenerateSamplesTask extends DefaultTask {
+
+    public static final String ASCIIDOC_BEGIN_TAG = "// tag::user_guide[]";
+    public static final String ASCIIDOC_END_TAG = "// end::user_guide[]";
 
     private final ObjectMapper mapper = new ObjectMapper();
 
@@ -47,8 +53,15 @@ abstract public class GenerateSamplesTask extends DefaultTask {
         RestClient restClient = new RestClient(getServiceUrl().get(), getHttpHeaders().get());
         for(Request request: requests) {
             try {
-                String response = restClient.call(request);
-                getLogger().lifecycle(response);
+                StringBuffer buff = new StringBuffer(ASCIIDOC_BEGIN_TAG);
+                buff.append('\n');
+                buff.append(format(restClient.call(request)));
+                buff.append('\n');
+                buff.append(ASCIIDOC_END_TAG);
+                buff.append('\n');
+                String response = buff.toString();
+                getLogger().info(response);
+                writeResponse(request, response);
             } catch (Exception e) {
                 getLogger().error(e.getMessage());
                 throw new RuntimeException("Build failed:", e);
@@ -72,5 +85,22 @@ abstract public class GenerateSamplesTask extends DefaultTask {
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
         JsonNode root = mapper.readTree(response);
         return mapper.writeValueAsString(root);
+    }
+
+    private Path outputFilePath(String fileName) {
+        RegularFile regularFile = getDestinationDirectory().file(fileName).get();
+        File javaIoFile = regularFile.getAsFile();
+        return Paths.get(javaIoFile.toURI());
+    }
+
+    private void writeResponse(Request request, String response) {
+        String fileName = String.format("%s-%s.jsonc", request.getType(), request.getId());
+        Path filePath = outputFilePath(fileName.replace('/', '-'));
+        try {
+            Files.writeString(filePath, response, StandardCharsets.UTF_8);
+            getLogger().info("Successfully wrote {} file.", filePath.toString());
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
