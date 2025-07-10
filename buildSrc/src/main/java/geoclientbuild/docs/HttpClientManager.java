58cc71aa86c61d694a47c44c88f225e253bd391f
Matthew Lipper <mlipper@gmail.com>
2025-07-10 13:26:08 -0400

    The actively maintained Geoclient project is here: https://github.com/mlipper/geoclient

--- buildSrc/src/main/java/geoclientbuild/docs/HttpClientManager.java
+++
@@ -1,9 +0,0 @@
-package geoclientbuild.docs;
-
-import java.io.IOException;
-
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-
-public interface HttpClientManager {
-    CloseableHttpClient getCloseableHttpClient() throws InterruptedException, IOException;
-}
--- images/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-"${GEOSUPPORT_HOME}/bin/geosupport" install
-
-exec java -Dspring.profiles.active=default \
-  org.springframework.boot.loader.launch.JarLauncher
-
